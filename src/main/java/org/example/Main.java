8e8162f594f2a8505cf6d14e3fd0841a06bd2197
root <pochtovik2048@gmail.com>
2023-08-10 20:06:01 +0300

    revolution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("lmao");
+        System.out.println("v for vendetta");
         int x = 1;
         System.out.println("Sieg Heil");
     }
