a89e33d253c5e5828416b15bb1a8dd774d524c56
viper <example@example.com>
2023-08-12 20:32:38 +0300

    толік волк проверил дз и внес предложение

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,24 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        AirStripe odess;
+        odess = new AirStripe(2800);
+        AirStripe nyw;
+        nyw = new AirStripe(4442);
+
+        // Кафешки
+        // todo: сделай так, что Love Is Coffee, Tour De France cafe Odesa & Aroma Kava бьіли разньіми об'єктами
+        Cafe ods;
+        ods = new Cafe("Love is Coffee, Tour de France cafe Odessa и конечно же Aroma Kava");
+        Cafe ny;
+        ny = new Cafe("Starbuck, Jean-Georges Vongerichten и так же самое известное из них Lisbon Cafe Lounge");
+
 // Аэропорт
-        Airports airportOd;
-        airportOd = new Airports("Odessa International Airport", 1961, 1000);
-        Airports newyork;
-        newyork = new Airports("John F. Kennedy International Airport",1948,35000);
+        Airport odesaAirport = new Airport("Odessa International Airport", 1961, 1000, new AirStripe[] {odess, nyw}, new Cafe[]{ods, });
+        Airport newYorkAirport = new Airport("John F. Kennedy International Airport", 1948, 35000, new AirStripe[]{nyw}, new Cafe[]{ny});
 // Самолёты
         Planes boeingsem;
         boeingsem = new Planes("Boeing 757-200",1982,65000000,200);
@@ -22,11 +34,6 @@ public class Main {
         muj = new Toilets("мужском",12,2);
         Toilets jen;
         jen = new Toilets("женском",20,6);
-// Кафешки
-        Cafes ods;
-        ods = new Cafes("Одесском",4,"Love is Coffee, Tour de France cafe Odessa и конечно же Aroma Kava");
-        Cafes ny;
-        ny = new Cafes("Нью-йоркском", 43,"Starbuck, Jean-Georges Vongerichten и так же самое известное из них Lisbon Cafe Lounge");
 // Теры
         Tudasuda odes;
         odes = new Tudasuda("Одесский", 2002,52);
@@ -38,37 +45,35 @@ public class Main {
         Pilots mish;
         mish = new Pilots("Одесса","Михаил","Чекмарёв",55,23);
 // Полосы
-        Polosi nyw;
-        nyw = new Polosi("Нью-Йорка",4,4442);
-        Polosi odess;
-        odess = new Polosi("Одессы",1,2800);
+
+        System.out.println(Arrays.toString(odesaAirport.presentAirStripes()));
+        System.out.println(Arrays.toString(odesaAirport.presentCafes()));
 
 
-
-        System.out.println("1 - " + "Аэропорты");
-        System.out.println(airportOd.presentAeroport());
-        System.out.println(newyork.presentAeroport());
-        System.out.println("2 - " + "Самолёты");
-        System.out.println(boeingsem.presentPlanes());
-        System.out.println(andva.presentPlanes());
-        System.out.println("3 - " + "Рейсы");
-        System.out.println(molcan.presentReysi());
-        System.out.println(ukrbel.presentReysi());
-        System.out.println("4 - " + "Туалеты");
-        System.out.println(muj.presentToilets());
-        System.out.println(jen.presentToilets());
-        System.out.println("5 - " + "Кафешки");
-        System.out.println(ods.presentCafes());
-        System.out.println(ny.presentCafes());
-        System.out.println("6 - " + "Террористы");
-        System.out.println(odes.presentTudasuda());
-        System.out.println(newy.presentTudasuda() + " (Ну вы конечно знаете парочку из них) ");
-        System.out.println("7 - " + "Пилоты");
-        System.out.println(olej.presentPilots() + " года");
-        System.out.println(mish.presentPilots()+ " года ");
-        System.out.println("8 - " + "Взлётные полосы");
-        System.out.println(nyw.presentPolosi());
-        System.out.println(odess.presentPolosu());
+//        System.out.println("1 - " + "Аэропорты");
+//        System.out.println(airportOd.presentAirport());
+//        System.out.println(newyork.presentAirport());
+//        System.out.println("2 - " + "Самолёты");
+//        System.out.println(boeingsem.presentPlanes());
+//        System.out.println(andva.presentPlanes());
+//        System.out.println("3 - " + "Рейсы");
+//        System.out.println(molcan.presentReysi());
+//        System.out.println(ukrbel.presentReysi());
+//        System.out.println("4 - " + "Туалеты");
+//        System.out.println(muj.presentToilets());
+//        System.out.println(jen.presentToilets());
+//        System.out.println("5 - " + "Кафешки");
+//        System.out.println(ods.presentCafes());
+//        System.out.println(ny.presentCafes());
+//        System.out.println("6 - " + "Террористы");
+//        System.out.println(odes.presentTudasuda());
+//        System.out.println(newy.presentTudasuda() + " (Ну вы конечно знаете парочку из них) ");
+//        System.out.println("7 - " + "Пилоты");
+//        System.out.println(olej.presentPilots() + " года");
+//        System.out.println(mish.presentPilots()+ " года ");
+//        System.out.println("8 - " + "Взлётные полосы");
+//        System.out.println(nyw.presentPolosi());
+//        System.out.println(odess.presentPolosu());
 
 
     }
